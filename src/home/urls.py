81153067c6c30fbb71d594839506555bb81588a0
Oscar Diaz <oscargdi@hotmail.com>
2024-01-10 23:51:14 +0000

    Add profile page and link in the navbar

--- src/home/urls.py
+++ src/home/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home-index"),
+    path("profile/", views.profile, name="home-profile"),
 ]
--- src/home/views.py
+++ src/home/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 
-# Create your views here.
 @login_required
 def home(request):
     return render(request, "home/index.html")
+
+
+@login_required
+def profile(request):
+    return render(request, "home/profile.html")
