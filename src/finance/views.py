1d9078ec916a7e90df48c4404eb5667ac6fcd590
Oscar Diaz <oscargdi@hotmail.com>
2024-01-13 05:10:15 +0000

    Add finance index page and update finance URLs and views

--- src/finance/views.py
+++ src/finance/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+from . import models
+def index(request: HttpRequest):
+    accounts = models.Account.objects.filter(user=request.user)
+    return render(request, "finance/index.html",context={"accounts": accounts})
