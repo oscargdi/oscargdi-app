b4a55bf35484b82e122fe0661bd34ed8452aa768
Oscar Diaz <oscargdi@hotmail.com>
2024-01-26 22:27:01 +0000

    Refactor views.py to remove unused import and add whitespace

--- src/finance/views.py
+++ src/finance/views.py
@@ -1,6 +1,8 @@
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest
 from django.shortcuts import render
 from . import models
+
+
 def index(request: HttpRequest):
     accounts = models.Account.objects.filter(user=request.user)
-    return render(request, "finance/index.html",context={"accounts": accounts})
+    return render(request, "finance/index.html", context={"accounts": accounts})
