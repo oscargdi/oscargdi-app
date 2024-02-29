6bf97fc9a877216b83f38aace658ac12100accdb
Oscar Diaz <oscargdi@hotmail.com>
2024-02-29 22:37:02 +0000

    Refactor async_send_message to use a specific queue name

--- src/home/tasks.py
+++ src/home/tasks.py
@@ -3,6 +3,6 @@ from dramatiq import actor
 from .services import send_message
 
 
-@actor
+@actor(queue_name="send_message")
 def async_send_message(title: str, message: str):
     send_message(title, message)
